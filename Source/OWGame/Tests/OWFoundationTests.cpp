839c88f6001102866924aa5a5c974d9bff72b14c
borlo11 <ameroni087@gmail.com>
2026-08-30 01:51:30 +0200

    Add foundation automation test

---
+++ Source/OWGame/Tests/OWFoundationTests.cpp
@@ -0,0 +1,24 @@
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "Misc/AutomationTest.h"
+#include "OWGameCharacter.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FOWCharacterDefaultsTest,
+    "OWGame.Foundation.CharacterDefaults",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FOWCharacterDefaultsTest::RunTest(const FString& Parameters)
+{
+    const AOWGameCharacter* CharacterCDO = GetDefault<AOWGameCharacter>();
+
+    TestNotNull(TEXT("Character CDO exists"), CharacterCDO);
+    if (CharacterCDO)
+    {
+        TestTrue(TEXT("Interaction range is positive"), CharacterCDO->GetInteractionRange() > 0.0f);
+    }
+
+    return true;
+}
+
+#endif
