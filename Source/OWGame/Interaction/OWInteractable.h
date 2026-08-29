c94b7a3932adca5a18a03ad844f34c4618e2d9b3
borlo11 <ameroni087@gmail.com>
2026-08-30 01:51:18 +0200

    Add reusable interaction interface

---
+++ Source/OWGame/Interaction/OWInteractable.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "OWInteractable.generated.h"
+
+UINTERFACE(BlueprintType)
+class OWGAME_API UOWInteractable : public UInterface
+{
+    GENERATED_BODY()
+};
+
+class OWGAME_API IOWInteractable
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+    bool CanInteract(AActor* Interactor) const;
+
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+    void Interact(AActor* Interactor);
+};
