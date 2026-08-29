2f67659d0e44adba2168821db09ae6ac0f5dcfc1
borlo11 <ameroni087@gmail.com>
2026-08-30 01:51:25 +0200

    Add third-person character foundation

---
+++ Source/OWGame/OWGameCharacter.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "OWGameCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class OWGAME_API AOWGameCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AOWGameCharacter();
+
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    virtual void BeginPlay() override;
+
+    UFUNCTION(BlueprintPure, Category="Interaction")
+    float GetInteractionRange() const { return InteractionRange; }
+
+protected:
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue& Value);
+    void StartJump();
+    void StopJump();
+    void TryInteract();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+    TObjectPtr<USpringArmComponent> CameraBoom;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+    TObjectPtr<UCameraComponent> FollowCamera;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputAction> LookAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputAction> JumpAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputAction> InteractAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera", meta=(ClampMin="100.0", ClampMax="1000.0"))
+    float CameraDistance = 350.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera", meta=(ClampMin="0.1", ClampMax="5.0"))
+    float LookSensitivity = 1.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Interaction", meta=(ClampMin="50.0", ClampMax="1000.0"))
+    float InteractionRange = 300.0f;
+
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+    UPROPERTY(EditAnywhere, Category="Debug")
+    bool bDrawInteractionTrace = false;
+#endif
+};
