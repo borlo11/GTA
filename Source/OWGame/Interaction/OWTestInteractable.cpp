4f8af0c3a100a783f491362c5be6ec3d1bb8c0ab
borlo11 <ameroni087@gmail.com>
2026-08-30 01:51:23 +0200

    Implement minimal test interactable actor

---
+++ Source/OWGame/Interaction/OWTestInteractable.cpp
@@ -0,0 +1,29 @@
+#include "Interaction/OWTestInteractable.h"
+#include "OWGame.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+AOWTestInteractable::AOWTestInteractable()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+    SetRootComponent(SceneRoot);
+
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    Mesh->SetupAttachment(SceneRoot);
+}
+
+bool AOWTestInteractable::CanInteract_Implementation(AActor* Interactor) const
+{
+    return IsValid(Interactor);
+}
+
+void AOWTestInteractable::Interact_Implementation(AActor* Interactor)
+{
+    bHasBeenInteractedWith = !bHasBeenInteractedWith;
+    UE_LOG(LogOWGame, Log, TEXT("Test interactable %s used by %s. State=%s"),
+        *GetName(),
+        IsValid(Interactor) ? *Interactor->GetName() : TEXT("None"),
+        bHasBeenInteractedWith ? TEXT("On") : TEXT("Off"));
+}
