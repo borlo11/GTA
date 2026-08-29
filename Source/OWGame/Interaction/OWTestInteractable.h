ab62215b7f6f473241b01d2bedea89bdc6f917c4
borlo11 <ameroni087@gmail.com>
2026-08-30 01:51:20 +0200

    Add minimal test interactable actor

---
+++ Source/OWGame/Interaction/OWTestInteractable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interaction/OWInteractable.h"
+#include "OWTestInteractable.generated.h"
+
+UCLASS()
+class OWGAME_API AOWTestInteractable : public AActor, public IOWInteractable
+{
+    GENERATED_BODY()
+
+public:
+    AOWTestInteractable();
+
+    virtual bool CanInteract_Implementation(AActor* Interactor) const override;
+    virtual void Interact_Implementation(AActor* Interactor) override;
+
+private:
+    UPROPERTY(VisibleAnywhere, Category="Interaction")
+    TObjectPtr<USceneComponent> SceneRoot;
+
+    UPROPERTY(VisibleAnywhere, Category="Interaction")
+    TObjectPtr<UStaticMeshComponent> Mesh;
+
+    UPROPERTY(VisibleInstanceOnly, Category="Interaction")
+    bool bHasBeenInteractedWith = false;
+};
