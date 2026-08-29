dbb24fa55f2713c3b88377c592bf32b235d118df
borlo11 <ameroni087@gmail.com>
2026-08-30 01:50:50 +0200

    Add base player controller

---
+++ Source/OWGame/OWGamePlayerController.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "OWGamePlayerController.generated.h"
+
+UCLASS()
+class OWGAME_API AOWGamePlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+public:
+    AOWGamePlayerController();
+};
