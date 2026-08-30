ad1bf3a971a82df5b78b73fcd54c653868264e32
borlo11 <ameroni087@gmail.com>
2026-08-30 22:32:02 +0200

    Spawn population manager at runtime

--- Source/OWGame/OWGameGameMode.cpp
+++ Source/OWGame/OWGameGameMode.cpp
@@ -2,10 +2,32 @@
 #include "OWGameCharacter.h"
 #include "OWGamePlayerController.h"
 #include "UI/OWGameHUD.h"
+#include "Population/OWPopulationManager.h"
+
+#include "Engine/World.h"
 
 AOWGameGameMode::AOWGameGameMode()
 {
     DefaultPawnClass = AOWGameCharacter::StaticClass();
     PlayerControllerClass = AOWGamePlayerController::StaticClass();
     HUDClass = AOWGameHUD::StaticClass();
+}
+
+
+void AOWGameGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (UWorld* World = GetWorld())
+    {
+        FActorSpawnParameters SpawnParameters;
+        SpawnParameters.SpawnCollisionHandlingOverride =
+            ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        World->SpawnActor<AOWPopulationManager>(
+            AOWPopulationManager::StaticClass(),
+            FVector::ZeroVector,
+            FRotator::ZeroRotator,
+            SpawnParameters);
+    }
 }
