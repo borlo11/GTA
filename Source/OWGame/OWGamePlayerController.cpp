0376c22932b0c8df71cc20becd927cb5205b0052
borlo11 <ameroni087@gmail.com>
2026-08-31 00:34:42 +0200

    Add prototype crime trigger and wanted component

--- Source/OWGame/OWGamePlayerController.cpp
+++ Source/OWGame/OWGamePlayerController.cpp
@@ -1,27 +1,68 @@
 #include "OWGamePlayerController.h"
 
 #include "OWGame.h"
+#include "Crime/OWWantedComponent.h"
+
+#include "Components/InputComponent.h"
 #include "GameFramework/Pawn.h"
+#include "InputCoreTypes.h"
 
 AOWGamePlayerController::AOWGamePlayerController()
 {
     bShowMouseCursor = false;
     bEnableClickEvents = false;
     bEnableMouseOverEvents = false;
+
+    WantedComponent = CreateDefaultSubobject<UOWWantedComponent>(TEXT("WantedComponent"));
 }
 
 void AOWGamePlayerController::BeginPlay()
 {
     Super::BeginPlay();
     ApplyGameplayInputMode();
-
 }
 
 void AOWGamePlayerController::OnPossess(APawn* InPawn)
 {
     Super::OnPossess(InPawn);
     ApplyGameplayInputMode();
+}
 
+void AOWGamePlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+#if !UE_BUILD_SHIPPING
+    if (InputComponent)
+    {
+        InputComponent->BindKey(
+            EKeys::F,
+            IE_Pressed,
+            this,
+            &AOWGamePlayerController::DebugReportCrime);
+    }
+#endif
+}
+
+void AOWGamePlayerController::ReportPrototypeCrime(int32 Severity)
+{
+    if (!WantedComponent || Severity <= 0)
+    {
+        return;
+    }
+
+    WantedComponent->ReportCrime(Severity);
+}
+
+void AOWGamePlayerController::DebugReportCrime()
+{
+    ReportPrototypeCrime(1);
+
+    UE_LOG(
+        LogOWGame,
+        Log,
+        TEXT("Prototype crime trigger pressed. Wanted=%d."),
+        WantedComponent ? WantedComponent->GetWantedLevel() : 0);
 }
 
 void AOWGamePlayerController::ApplyGameplayInputMode()
