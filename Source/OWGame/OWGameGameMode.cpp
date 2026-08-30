b7cfc5f3b11601418fd90264945b42ac649a00f9
borlo11 <ameroni087@gmail.com>
2026-08-30 21:47:39 +0200

    Configure contextual HUD

--- Source/OWGame/OWGameGameMode.cpp
+++ Source/OWGame/OWGameGameMode.cpp
@@ -1,9 +1,11 @@
 #include "OWGameGameMode.h"
 #include "OWGameCharacter.h"
 #include "OWGamePlayerController.h"
+#include "UI/OWGameHUD.h"
 
 AOWGameGameMode::AOWGameGameMode()
 {
     DefaultPawnClass = AOWGameCharacter::StaticClass();
     PlayerControllerClass = AOWGamePlayerController::StaticClass();
+    HUDClass = AOWGameHUD::StaticClass();
 }
