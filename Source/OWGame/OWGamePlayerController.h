02e26e0614e9a30fd800cb9d2c69318b083893f9
borlo11 <ameroni087@gmail.com>
2026-08-31 00:34:40 +0200

    Own M5 wanted state on persistent player controller

--- Source/OWGame/OWGamePlayerController.h
+++ Source/OWGame/OWGamePlayerController.h
@@ -4,6 +4,8 @@
 #include "GameFramework/PlayerController.h"
 #include "OWGamePlayerController.generated.h"
 
+class UOWWantedComponent;
+
 UCLASS()
 class OWGAME_API AOWGamePlayerController : public APlayerController
 {
@@ -12,10 +14,21 @@ class OWGAME_API AOWGamePlayerController : public APlayerController
 public:
     AOWGamePlayerController();
 
+    UFUNCTION(BlueprintPure, Category="Crime")
+    UOWWantedComponent* GetWantedComponent() const { return WantedComponent; }
+
+    UFUNCTION(BlueprintCallable, Category="Crime")
+    void ReportPrototypeCrime(int32 Severity = 1);
+
 protected:
     virtual void BeginPlay() override;
     virtual void OnPossess(APawn* InPawn) override;
+    virtual void SetupInputComponent() override;
 
 private:
     void ApplyGameplayInputMode();
+    void DebugReportCrime();
+
+    UPROPERTY(VisibleAnywhere, Category="Crime")
+    TObjectPtr<UOWWantedComponent> WantedComponent;
 };
